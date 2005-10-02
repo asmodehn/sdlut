91add2f6d3886a06f1c5d1e4cf445e82fd439244
AlexV <asmodehn@gmail.com>
2005-10-02 06:38:07 +0000

    added  a class to handle the display and the WM from the OS...

---
+++ include/Display.hh
@@ -0,0 +1,9 @@
+#ifndef DISPLAY_HH
+#define DISPLAY_HH
+
+class Display
+{
+};
+
+#endif
+
---
+++ test/test.cc
@@ -0,0 +1,23 @@
+//test program
+#include "AML.hh"
+
+//tmp : logger is aimed only for internal use in AML
+#include "Logger.hh"
+
+//would be nice if we can put that in AML.hh
+#ifdef __MINGW32__
+#ifdef main
+#undef main
+#endif
+#endif
+
+
+int main(int argc, char* argv [] )
+{
+	
+		Logger testlog;
+		
+		testlog.add ("blablbla");
+		
+		return 0;
+};
