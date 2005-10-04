f7f210414fb7e291f4a52baa963223da9ee27fc8
AlexV <asmodehn@gmail.com>
2005-10-04 21:58:31 +0000

    implemented new Logger

--- include/Logger.hh
+++ include/Logger.hh
@@ -5,20 +5,75 @@
 
 #include <iostream>
 
+/**
+ * \class Logger
+ *
+ * \brief This class defines loglevel on top of clog
+ *
+ * This Logger use indentation levels, and also filters using 3 loglevel : quiet / normal / verbose
+ *
+ * \author Alex
+ *
+ * \date 2005/10/05
+ *
+ * Contact: asmodehn@gna.org
+ *
+ */
+
+
+typedef enum {quiet,normal,verbose} Loglevel;
+
 class Logger
 {
-	std::ostream& _ostr;
+  Loglevel _loglevel;
+	int _indentwidth;
 	
   public :
-    Logger(std::ostream & ostr = std::cout);
+  
+    ///Default Constructor that defines the loglevel of messages stored
+    Logger(Loglevel lvl = verbose, int indentwidth = 2);
     //TODO Logger (std::string filename);
+    ///Default Destructor that flush the Log Buffer
     ~Logger();
     
-    bool add(std::string);
+    bool add(std::string, Loglevel lvl);
+    bool add(int indentlevel, std::string, Loglevel lvl);
 	
-    bool flush(void);
+    void flush(void);
     
     
 };
+
+inline Logger::Logger(Loglevel lvl, int indentwidth)
+: _loglevel(lvl), _indentwidth(indentwidth)
+{
+	
+}
+
+inline bool Logger::add(std::string msg,Loglevel lvl)
+{
+	bool getmsg = (lvl <=_loglevel);
+	if ( getmsg )
+    std::clog << msg;
+	return getmsg;
+}
+
+inline bool Logger::add(int indentlevel, std::string msg,Loglevel lvl)
+{
+  std::string s(indentlevel * _indentwidth, ' ');
+	std::clog << s;
+	return add(msg, lvl);
+}
+
+inline void Logger::flush(void)
+{
+	std::clog.flush();
+}
+
+inline Logger::~Logger()
+{
+	flush();
+}
+
 
 #endif
--- test/test.cc
+++ test/test.cc
@@ -17,7 +17,9 @@ int main(int argc, char* argv [] )
 	
 		Logger testlog;
 		
-		testlog.add ("blablbla");
+		testlog.add (4,"blablabla",verbose);
 		
 		return 0;
-};
+}
+
+
