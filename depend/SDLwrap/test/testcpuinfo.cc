1b3d030df72f4e3040943ccd46fd2fc0fbd34d39
AlexV <asmodehn@gmail.com>
2005-11-08 09:40:42 +0000

    added testcpuinfo test

---
+++ depend/SDLwrap/test/testcpuinfo.cc
@@ -0,0 +1,28 @@
+
+/* Test program to check SDL's CPU feature detection */
+
+#include <stdio.h>
+
+#include "SDLwrap.hh"
+#include "SDL_cpuinfo.h"
+
+
+//would be nice if we can put that in SDLwrap.hh
+#ifdef __MINGW32__
+#ifdef main
+#undef main
+#endif
+#endif
+
+int main(int argc, char *argv[])
+{
+	printf("RDTSC %s\n", SDL_HasRDTSC() ? "detected" : "not detected");
+	printf("MMX %s\n", SDL_HasMMX() ? "detected" : "not detected");
+	printf("MMX Ext %s\n", SDL_HasMMXExt() ? "detected" : "not detected");
+	printf("3DNow %s\n", SDL_Has3DNow() ? "detected" : "not detected");
+	printf("3DNow Ext %s\n", SDL_Has3DNowExt() ? "detected" : "not detected");
+	printf("SSE %s\n", SDL_HasSSE() ? "detected" : "not detected");
+	printf("SSE2 %s\n", SDL_HasSSE2() ? "detected" : "not detected");
+	printf("AltiVec %s\n", SDL_HasAltiVec() ? "detected" : "not detected");
+	return(0);
+}
