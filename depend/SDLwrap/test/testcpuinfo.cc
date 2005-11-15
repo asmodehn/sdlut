b8b00f3e7c7744d8911ea8d8b43c54244451e2a7
AlexV <asmodehn@gmail.com>
2005-11-15 22:42:26 +0000

    added version detection

--- depend/SDLwrap/test/testcpuinfo.cc
+++ depend/SDLwrap/test/testcpuinfo.cc
@@ -3,8 +3,14 @@
 
 #include <stdio.h>
 
+//TODO : MINIMUM VERSION TO BUILD THIS TEST...
+
+
 #include "SDLwrap.hh"
+
+#if SDL_VERSION_ATLEAST(1, 2, 7)
 #include "SDL_cpuinfo.h"
+#endif
 
 
 //would be nice if we can put that in SDLwrap.hh
@@ -16,6 +22,8 @@
 
 int main(int argc, char *argv[])
 {
+
+#if SDL_VERSION_ATLEAST(1, 2, 7)
 	printf("RDTSC %s\n", SDL_HasRDTSC() ? "detected" : "not detected");
 	printf("MMX %s\n", SDL_HasMMX() ? "detected" : "not detected");
 	printf("MMX Ext %s\n", SDL_HasMMXExt() ? "detected" : "not detected");
@@ -24,5 +32,13 @@ int main(int argc, char *argv[])
 	printf("SSE %s\n", SDL_HasSSE() ? "detected" : "not detected");
 	printf("SSE2 %s\n", SDL_HasSSE2() ? "detected" : "not detected");
 	printf("AltiVec %s\n", SDL_HasAltiVec() ? "detected" : "not detected");
+
+#else
+
+SDL_version compiled;
+  SDL_VERSION(&compiled);
+	printf("You SDL compiled version: %d.%d.%d doesnt support the cpuInfo feature\n",
+			compiled.major, compiled.minor, compiled.patch);
+#endif
 	return(0);
 }
