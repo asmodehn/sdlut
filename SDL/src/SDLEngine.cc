a9de10118163b3a444063ded75f14ff13a53f902
AlexV <asmodehn@gmail.com>
2006-04-03 23:00:28 +0000

    cleaning a bit the defaultGLengine, start implementing the testGL

--- SDL/src/SDLEngine.cc
+++ SDL/src/SDLEngine.cc
@@ -52,44 +52,13 @@ namespace RAGE
 		void DefaultGLEngine::render(VideoSurface& screen) const
 		{
 
-			//3D Rendering
-
-//glEnable( GL_DEPTH_TEST ) ;
-//glMatrixMode( GL_PROJECTION ) ;
-//glLoadIdentity() ;
-//
-//			glMatrixMode(GL_PROJECTION);      // Select The Projection Matrix
-//			glLoadIdentity();         // Reset The Projection Matrix
-//	
-//			GLdouble znear = 0.1f ;
-//			GLdouble zfar = 100.0f ;
-//			GLdouble ymax = znear * tan(45.0f * M_PI / 360.0);
-//			GLdouble ymin = -ymax;
-//			GLdouble xmin = ymin * GLfloat(width) / GLfloat(height);
-//			GLdouble xmax = ymax * GLfloat(width) / GLfloat(height);
-//	
-//			glFrustum(xmin, xmax, ymin, ymax, znear, zfar);
-
-
 			//2D Rendering
 
-glDisable( GL_DEPTH_TEST ) ;
-glMatrixMode( GL_PROJECTION ) ;
-glLoadIdentity() ;
-//
-///*
-// * Upside-down square viewport : it maps the screen as if the (arbitrary-set) resolution were
-// * 1000x1000 pixels.
-// *
-// */
-//
-glOrtho( /* left */ 0, /* right */ screen.getWidth(), /* bottom */ screen.getHeight(), /* top */ 0, /* near */ 0, /* far */ 1 ) ;
-//- or, preferably to keep the 4/3 ratio like 800x600, 640x480, 1024x768, etc. - 
-//
-//// Non-reversed 4/3 viewport :	
-//glOrtho( /* left */ -320.0f, /* right */ 320.0f, /* bottom */ -240.0f, /* top */ 240.0f, 
-//	/* near */ -1, /* far */ 1 ) ;
-////use one or the other depending if your texture is flipped or not vertically
+			glDisable( GL_DEPTH_TEST ) ;
+			glMatrixMode( GL_PROJECTION ) ;
+			glLoadIdentity() ;
+
+			glOrtho( 0, screen.getWidth(), screen.getHeight(), 0, 0, 1 ) ;
 			glMatrixMode(GL_MODELVIEW);
 			glLoadIdentity();
 
@@ -99,29 +68,23 @@ glOrtho( /* left */ 0, /* right */ screen.getWidth(), /* bottom */ screen.getHei
 			//Load the texture
 			glBindTexture(GL_TEXTURE_2D, imagetexture);
 
-//glColor4f( 1.0, 1.0, 1.0, 1.0 ) ;
-glDisable(GL_CULL_FACE);
-
-glBegin( GL_QUADS ) ;
-	glTexCoord2i( 0,0) ;
-	glVertex2i( screen.getWidth() - image->getWidth() , screen.getHeight()- image->getHeight() ) ;
+			glBegin( GL_QUADS ) ;
+				glTexCoord2i( 0,0) ;
+				glVertex2i( screen.getWidth() - image->getWidth() , screen.getHeight()- image->getHeight() ) ;
 	
-	glTexCoord2i( 0, 1) ;
-	glVertex2i( screen.getWidth() - image->getWidth() , screen.getHeight()) ;
+				glTexCoord2i( 0, 1) ;
+				glVertex2i( screen.getWidth() - image->getWidth() , screen.getHeight()) ;
 	
-	glTexCoord2i( 1, 1) ;
-	glVertex2i( screen.getWidth() , screen.getHeight()) ;
-	
-	glTexCoord2i( 1, 0) ;
-	glVertex2i( screen.getWidth(), screen.getHeight() - image->getHeight() ) ;
-glEnd() ;
+				glTexCoord2i( 1, 1) ;
+				glVertex2i( screen.getWidth() , screen.getHeight()) ;
+				
+				glTexCoord2i( 1, 0) ;
+				glVertex2i( screen.getWidth(), screen.getHeight() - image->getHeight() ) ;
+			glEnd() ;
 
 
 			//Disable texturing
 			glDisable(GL_TEXTURE_2D);
-
-
-
 		}
 
 			//to initialise the engine, just called once before any render
