e7a1f714474d2184e8f42e2ac8900503d7fb670a
AlexV <asmodehn@gmail.com>
2006-06-13 18:38:57 +0000

    started to implement correctly the timer callback... complete new design with functor. build but still not working...

---
+++ SDL/test/SDLtestTimer.cc
@@ -0,0 +1,50 @@
+
+/* Test program to test the timer callbacks
+*/
+
+#include <iostream>
+#include "SDL.hh"
+using namespace RAGE;
+
+class ObjectWithCallback
+{
+
+
+	public:
+	bool called;
+		
+	ObjectWithCallback() : called(false) {}
+		
+	unsigned int callback(unsigned int interval, void* args)
+	{
+		called = !called;
+		std::cout << "Called changed to : " << called << std::endl;
+	}
+};
+
+
+int main(int argc, char *argv[])
+{
+	Logger testlog("testver");
+
+	testlog << "SDL init..." << std::endl;
+
+	SDL::App::getInstance();
+			
+	testlog << "Creating object with callback after 2 seconds" << std::endl;
+	ObjectWithCallback obj;
+
+	SDL::Timer<ObjectWithCallback> timer;
+
+	timer.setInterval(200);
+	timer.setCallback(&obj,&ObjectWithCallback::callback,(void*)NULL);
+
+	timer.start();
+
+	while (!obj.called)
+		SDL::Delay(1); //TODO Display time running
+
+	testlog << "Called back : " << obj.called << std::endl;
+
+	return 0;
+}
