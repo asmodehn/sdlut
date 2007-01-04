d7ebcb93011a1c91412930ecf8c7b1b8617341a6
XorfacX <xorfacx@gmail.com>
2007-01-04 20:00:42 +0000

    Trying to make a more complete sound test but the togglechannel() method dont seems to work as expected.

--- SDLut/test/SDLtestAudio.cc
+++ SDLut/test/SDLtestAudio.cc
@@ -30,8 +30,21 @@ int main(int argc, char *argv[])
 		SDL::App::getInstance().getMixer()->Play();
 		SDL::Delay(6000);
 	}
-	//otherwise we dont use any.
-
+	//otherwise we dont use any and play sample sound
+	else
+	{
+		testlog << nl<<  "loading sound";
+		SDL::Sound wavefile("../../test/data/sample.wav");
+		testlog << nl<<  "setting mixer channels";
+		int wav_channel_1 = SDL::App::getInstance().getMixer()->mixSound(wavefile, false);
+		int wav_channel_2 = SDL::App::getInstance().getMixer()->mixSound(wavefile, false);
+		testlog << nl<<  "playing channel 1";
+		SDL::App::getInstance().getMixer()->toggleChannel(wav_channel_1);
+		SDL::Delay(1000);
+		testlog << nl<<  "playing channel 2";
+		SDL::App::getInstance().getMixer()->toggleChannel(wav_channel_2);
+		SDL::Delay(6000);
+	}
 
 	return 0;
 }
