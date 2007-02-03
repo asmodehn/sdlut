10f7c57d32d4b083d82bd73f0470a8a493b0d9da
XorfacX <xorfacx@gmail.com>
2007-02-03 22:49:07 +0000

    Project 0 2D v0.03:
    Solve a bug in the ini manager: where two options have the same beginning, it now doesn't take the first option but the good one.

--- P0/src/Ini_Manager.cpp
+++ P0/src/Ini_Manager.cpp
@@ -58,40 +58,45 @@ string Ini_Manager::Get_Option_String(const string &filename, const char* Option
 		newline_str = new string(newline);
 		if (newline_str->find("#", 0) == string::npos ) //Only check un-commented line
 		{
-			if ( newline_str->find(Option_Name, 0) != string::npos ) //find the good line
+			//find the option string is in this line
+			if ( newline_str->find(Option_Name, 0) != string::npos )
 			{
-				string::size_type equal_loc = newline_str->find("=", 0);  //find the '='
-				if ( equal_loc != string::npos )
+				//Check if after the option name there is a space (for option name that have the same beginning)
+				if ( newline_str->substr( ((string)Option_Name).length() , 1) == " " )
 				{
-					//find if string is between " "
-					string::size_type quote_1_loc = newline_str->find("\"", 0);
-					
-					if ( quote_1_loc == string::npos ) //no " found
+					string::size_type equal_loc = newline_str->find("=", 0);  //find the '='
+					if ( equal_loc != string::npos )
 					{
-						char* temp_res = new char;
-						//remove whitespace
-						std::stringstream( newline_str->substr(equal_loc+1) ) >> skipws >> temp_res;
-						//return the final clear string
-						fi.close();
-						return (string)temp_res;
-					}
-					else
-					{
-						//find the 2nd quote
-						string::size_type quote_2_loc = (newline_str->substr(quote_1_loc+1)).find("\"", 0);
-						if ( quote_2_loc == string::npos ) //the 2nd " wasn't found
+						//find if string is between " "
+						string::size_type quote_1_loc = newline_str->find("\"", 0);
+						
+						if ( quote_1_loc == string::npos ) //no " found
 						{
+							char* temp_res = new char;
+							//remove whitespace
+							std::stringstream( newline_str->substr(equal_loc+1) ) >> skipws >> temp_res;
+							//return the final clear string
 							fi.close();
-							//return the all string after the = (even with the only ")
-							return newline_str->substr(equal_loc+1);
-						} else {
-							fi.close();
-							//we return the string between the " "
-							return newline_str->substr(quote_1_loc+1, quote_2_loc);
+							return (string)temp_res;
+						}
+						else
+						{
+							//find the 2nd quote
+							string::size_type quote_2_loc = (newline_str->substr(quote_1_loc+1)).find("\"", 0);
+							if ( quote_2_loc == string::npos ) //the 2nd " wasn't found
+							{
+								fi.close();
+								//return the all string after the = (even with the only ")
+								return newline_str->substr(equal_loc+1);
+							} else {
+								fi.close();
+								//we return the string between the " "
+								return newline_str->substr(quote_1_loc+1, quote_2_loc);
+							}
 						}
 					}
 				}
-				}
+			}
 		}
 		//free before looping
 		//delete(newline_str);
