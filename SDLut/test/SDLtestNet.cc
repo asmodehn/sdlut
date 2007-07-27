5be43f641ac31a504584ab0e811e06539faa2aa7
AlexV <asmodehn@gmail.com>
2007-07-27 20:11:13 +0000

    quick fix of the test for SDL_net uses. should be improved...

--- SDLut/test/SDLtestNet.cc
+++ SDLut/test/SDLtestNet.cc
@@ -15,28 +15,29 @@ int main(int argc, char** argv)
     //Setup example
 
     App::getInstance().setName ("RAGE::SDL test - Network");
-    
-	
+
+
 	Log << nl << App::getInstance().init();
-	
+
 	Log << nl << App::getInstance().initNet();
 
 	SocketTCP serversocket(6666);
 
 	Log << nl <<serversocket;
-			
+
 	SocketTCP clientsocket("127.0.0.1",6666);
 
 	Log << nl <<clientsocket;
 
 	std::auto_ptr<SocketTCP> comsocket = serversocket.accept();
-	
-	std::string msg = "youhou !";
-	int size = sizeof(msg);
-	clientsocket.send(msg.c_str(),size);
 
-	char * rcvdmsg;
-	comsocket->receive(rcvdmsg,size);
+	std::string msg = "youhou !";
+	int len = msg.length();
+	Log << nl<< "msg len :" << len;
+	clientsocket.send(msg.c_str(),len);
+
+	char * rcvdmsg = new char[len] ;
+	comsocket->receive(rcvdmsg,len);
 
 	Log << nl << "Message : " << rcvdmsg;
 	return 0;
