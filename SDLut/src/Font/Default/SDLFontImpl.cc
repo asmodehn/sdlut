5b24d7f3e568c01bbcaab92f17d6c36c6f36706f
AlexV <asmodehn@gmail.com>
2009-01-04 09:24:40 +0000

    logging information trying to debug the font system...

--- SDLut/src/Font/Default/SDLFontImpl.cc
+++ SDLut/src/Font/Default/SDLFontImpl.cc
@@ -52,6 +52,7 @@ namespace SDL
 			{
 
 				std::auto_ptr<SDL_Surface> result(SDL_CreateRGBSurface(SDL_SWSURFACE, getSize(text).getw(), getSize(text).geth(), 16, BaseSurface::r_default_mask, BaseSurface::g_default_mask, BaseSurface::b_default_mask, BaseSurface::a_default_mask));
+				//Log << getSize(text);
 				for (unsigned int i= 0; i< text.size(); i++)
 				{
 					SDL_BlitSurface(const_cast<SDL_Surface*>(&_fontsurf.get_rSDL()),const_cast<SDL_Rect*>(alphalookup[text[i]].get_pSDL()),result.get(),const_cast<SDL_Rect*>(Rect(0,i*14,14,16).get_pSDL()));
