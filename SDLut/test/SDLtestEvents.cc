0869ec27041724b9aa635e317bbb69053ecf4338
AlexV <asmodehn@gmail.com>
2009-02-06 12:42:36 +0000

    added a new test to test Events system... not functional yet

---
+++ SDLut/test/SDLtestEvents.cc
@@ -0,0 +1,297 @@
+#include "SDL.hh"
+#include "Logger.hh"
+using namespace RAGE;
+using namespace RAGE::SDL;
+
+Logger Log("Test Events");
+
+class Console
+{
+
+	friend class MyEngine;
+
+	//if member embedded (object not reference), error when destructing Font...
+	//to debug -> copy / deletion problem
+	const Font _font;
+
+	Color _bgColor;
+
+	RGBSurface * surf;
+
+	std::string text;
+
+public :
+	Console(const Font & fnt = Font(),Color c = Color(0,0,0)) :_font(fnt),_bgColor(c),surf(NULL),text(">")
+	{
+		//init();
+		//draw();
+	}
+
+	bool init(int width, int height)
+	{
+		surf = new RGBSurface(_bgColor,width,height,8); //8 as magic number for minimum bpp.
+		if (surf == NULL) return false;
+		surf->optimise(); // Doesnt matter anyway we match the display one here.
+		return true;
+	}
+
+	//WARNING : BUG here if trying to set to fullscreen using F6, then error, then trying to resize -> crash
+	bool resize(int width, int height,bool keepcontent = true)
+	{
+		bool res = surf->resize(width,height,false);
+		draw();
+		return res;
+	}
+
+	void print(const std::string & newtext)
+	{
+		text += newtext;
+		draw();
+	}
+
+	void add(char newchar)
+	{
+		if (newchar != '\0')
+			text += newchar;
+		draw();
+	}
+
+	void draw()
+	{
+		std::auto_ptr<RGBSurface> textsurf = _font.render(text,Color(255,255,255),Font::Solid);
+		assert(textsurf.get());
+		surf->fill(_bgColor);
+		surf->blit(*textsurf);
+	}
+
+	~Console()
+	{
+		if (surf != NULL)
+			delete surf,surf = NULL;
+	}
+
+
+};
+
+//Defining general EventHandler
+class MyGeneralHandler : public GeneralHandler
+{
+	friend class EventManager;
+        bool _quitRequested;
+
+        public:
+
+            GeneralHandler() : _quitRequested(false)
+            {}
+            virtual ~GeneralHandler()
+            {}
+
+            //Callbacks on Window / Display events
+            virtual bool handleActiveEvent(bool gain, bool active, bool inputfocus, bool mousefocus)
+		{
+			return false;
+		}
+
+            virtual bool handleResizeEvent(int w, int h)
+		{
+			App::getInstance().getWindow().resizeDisplay(w,h);
+        		return true;
+		}
+            virtual bool handleExposeEvent()
+		{
+			 return false;
+		}
+            //callback on platform-dependent windows manager event
+            virtual bool handleSysWMEvent(void)
+    {
+        return false;
+    }
+
+            //Callback on other Events
+	    virtual bool handleUserEvent(Event::Type type, int code, void* data1, void* data2)
+    {
+        return false;
+    }
+
+
+            //Callback on Quit Event
+            virtual bool handleQuitEvent(void)
+		{
+        		_quitRequested=true;
+        		return true;
+		}
+            //Catch-all callback
+            virtual bool handleEvent(Event &event)
+		{
+#if (DEBUG ==2)
+        //Getting the details of the Event
+        Log << nl << "Last chance handler : " << cevent.getType() << std::endl;
+        return true;
+#else
+
+        return false;
+#endif
+		}
+
+};
+
+//Defining UserInput
+class MyUserInput : public TextInput
+{
+public:
+
+	Console *cons;
+
+	MyUserInput() : TextInput(), cons(NULL) {}
+
+	void setConsole (Console * newcons)
+	{
+		cons = newcons;
+	}
+
+    virtual bool handleKeyEvent (const Sym &s, bool pressed)
+    {
+        bool res = false;
+            switch( s.getKey() )
+            {
+                case KEscape:
+                if (pressed==false)
+                {
+#ifdef DEBUG
+                    Log << nl << "Quit requested !" << std::endl;
+#endif
+
+                    _quitRequested=true;
+                    res=true;
+                }
+                break;
+                case KF5:
+                if (pressed==true)
+                    App::getInstance().getWindow().iconify();
+                res = true;
+                break;
+				case KF6:
+                if (pressed==true)
+                    App::getInstance().getWindow().setFullscreen(!App::getInstance().getWindow().isFullscreen());
+                res = true;
+                break;
+				default: if (pressed == true) cons->add(s.getChar());
+                res = true;
+            }
+        return res;
+    }
+};
+
+class MyEngine : public DefaultEngine
+{
+
+public:
+
+	Point consolePos;
+	Console * console;
+	std::auto_ptr<RGBSurface> HelpMsg;
+		
+	MyEngine() : consolePos(0,0), console (NULL), HelpMsg(0)
+	{}
+
+	void setConsole( Console * cons ) { console = cons;}
+
+    virtual ~MyEngine()
+	{
+	}
+
+	bool init(int width, int height)
+	{
+		DefaultEngine::init(width,height);
+		console->init(width,height - 2 * DefaultEngine::_logo.getHeight());
+		consolePos.sety(DefaultEngine::_logo.getHeight());
+		HelpMsg = console->_font.render("Plz Use Keyboard To Write Text Down", Color(0xFF, 0xFF, 0xFF), Font::Shaded, Color(0, 0, 0));
+		return true;
+	}
+
+	bool resize(int width, int height)
+	{
+		DefaultEngine::resize(width,height);
+		console->resize(width,height - 2 * DefaultEngine::_logo.getHeight());
+		consolePos.sety(DefaultEngine::_logo.getHeight());
+		return true;
+	}
+
+	void prerender()
+	{
+		//nothing for now
+	}
+
+	void render(VideoSurface & screen) const
+    {
+		DefaultEngine::render(screen);
+
+		if (HelpMsg.get()) 
+			screen.blit( *HelpMsg, Point::Point(5, 5) );
+
+		if (console !=NULL)
+			screen.blit(*(console->surf),consolePos);
+    }
+
+	void postrender()
+	{
+		//nothing for now
+	}
+};
+
+
+//Main Program
+int main(int argc, char** argv)
+{
+
+    Logger testlog("Test Log");
+
+    //Setup example
+
+    testlog << nl << " Enabling SDL Video... " << std::endl;
+	App::getInstance().setName ("RAGE::SDL test - Font");
+    App::getInstance().initVideo(false,false,false,false);
+	App::getInstance().initText();
+
+    testlog << nl << " Creating the User Interface... " << std::endl;
+ 
+    App::getInstance().getWindow().setBGColor(Color (64,0,0));
+
+    Font font;
+    
+    if (argc > 1)
+    {
+	//specific font
+	font.setTTF(argv[1],24);
+    }
+
+	Console cons(font);
+	MyEngine engine;
+	engine.setConsole(&cons);
+	
+   //UI Creation
+    MyUserInput ui;
+	ui.setConsole(&cons);
+    MyGeneralHandler gh;
+    App::getInstance().getWindow().getEventManager().setKeyboard(&ui);
+   App::getInstance().getWindow().getEventManager().setGeneralHandler(&gh);
+
+
+	//without this line the default engine is used
+    App::getInstance().getWindow().setEngine(&engine);
+
+
+    if (! (App::getInstance().getWindow().resetDisplay()))
+    {
+        testlog << nl << "Display Creation FAILED !"<< std::endl;
+        exit(0);
+    }
+    else
+    {
+        App::getInstance().getWindow().mainLoop();
+    }
+    
+    return 0;
+}
+
+
