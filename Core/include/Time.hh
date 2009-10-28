2bfec93f9f8b1a11ef9dfad5a3d38c99b0f6ad39
AlexV <asmodehn@gmail.com>
2009-10-28 13:24:01 +0000

    first proper implementation of time and date classes. needs to be tested

--- Core/include/Time.hh
+++ Core/include/Time.hh
@@ -1,31 +1,150 @@
 #ifndef TIME_HH
 #define TIME_HH
 
-#include <time.h>
+#include <ctime>
+#include <string>
 
-void sleep (unsigned int sec);
+namespace Core
+{
+
+class Time;
+
+class Date
+{
+    friend class Time;
+
+    struct tm d;
+
+    ///explicit constructor to allow Time to produce Dates
+    explicit Date( struct tm * cdate ) : d(*cdate) {};
+
+public:
+    Date(int year =0, int month =0, int mday =0, int hour =0, int min =0, int sec =0 );
+    ~Date();
+
+    //TODO : improve that
+
+    ///Seconds.	[0-60] (1 leap second)
+    int & sec()
+    {
+        return d.tm_sec;
+    }
+    ///Minutes.	[0-59]
+    int & min()
+    {
+        return d.tm_min;
+    }
+    ///Hours.	[0-23]
+    int & hour()
+    {
+        return d.tm_hour;
+    }
+    ///Day.		[1-31]
+    int & mday()
+    {
+        return d.tm_mday;
+    }
+    ///Month.	[0-11] ///BEWARE
+    int & mon()
+    {
+        return d.tm_mon;
+    }
+    ///Year	- 1900. ///BEWARE
+    int & year()
+    {
+        return d.tm_year;
+    }
+    ///Day of week.	[0-6]
+    int & wday()
+    {
+        return d.tm_wday;
+    }
+    /// Days in year.[0-365]
+    int & yday()
+    {
+        return d.tm_yday;
+    }
+    ///DST.		[-1/0/1]
+    int & isdst()
+    {
+        return d.tm_isdst;
+    }
+    ///Timezone character code
+    std::string zone()
+    {
+        return std::string(d.tm_zone);
+    }
+
+    ///returns a textual version of the time
+    const std::string ascii();
+
+    Time mkTime();
+
+    //TODO : size_t strftime( char *str, size_t maxsize, const char *fmt, struct tm *time );
+};
+
+
 
 /** TODO : Class to handle portable calls to standard time functions
- * Pattern : transparent Singleton
+
 **/
-class RunTime
+
+class Time
 {
-    static RunTime * instance;
+    friend class Date ;
+    explicit Time (time_t tmt) : tt(tmt) {};
+
 
 public :
 
+//return the number of seconds ( warning : precision loss ) the  program has been running.
+// or the number of clock cycles if sec == false
+// 0  as a return code means we cannot access this information
+    static unsigned long clock(bool sec = true);
+
+//pause the execution of the current thread
+    static void sleep(unsigned int sec);
+
+    static Time now();
+
+private:
+
+    time_t tt;
+
+public :
+
+    Time();
+    Time( Date & d );
+    ~Time();
+
+    std::string localascii();
+
+    //return current localtime
+    Date local();
+
+    //return current GMT time
+    Date GMT();
+
+//TODO setlocale...
+
+
+
+
 };
 
+//TODO : check that to handle custom dates and conversion : http://c.developpez.com/sources/?page=III#DATE_mktime
 /** TODO : Class to handle Simulated time and relation with runtime...
- * Pattern : Transparent Singleton
+ * Pattern :
 **/
 
 class SimTime
 {
-    static SimTime * instance;
+    unsigned int rtfactor;
 
 public :
 
 };
+
+}// Core
 
 #endif
--- Core/src/Time.cc
+++ Core/src/Time.cc
@@ -1,8 +1,59 @@
 #include "Time.hh"
 
-
-void sleep (unsigned int sec)
+namespace Core
 {
+
+/********** Date ***********/
+Date::Date(int year , int month , int mday , int hour , int min , int sec  )
+{
+    //recomendd for safety
+    struct tm zerod = {0} ;
+
+    zerod.tm_sec = sec;
+    zerod.tm_min = min;
+    zerod.tm_hour = hour;
+    zerod.tm_mday = mday;
+    zerod.tm_mon = month -1; //month starts at 0;
+    zerod.tm_year = year -1900; //year starts at 1900 ( EPOCH )
+
+    d = zerod;
+}
+
+Date::~Date()
+{}
+
+
+const std::string Date::ascii()
+{
+    return std::string(asctime(&d));
+}
+
+Time Date::mkTime()
+{
+    return Time(mktime( &d));
+}
+
+/************ Time *************/
+
+Time::Time()
+{
+    time(&tt);
+}
+
+Time::Time( Date & d )
+{
+
+    tt = mktime (& d.d);
+
+}
+
+Time::~Time()
+{
+}
+
+void Time::sleep (unsigned int sec)
+{
+
 #ifdef _WIN32
     Sleep (sec * 1000);
 #else
@@ -16,6 +67,7 @@ void sleep (unsigned int sec)
     {
         while ((end - start) / CLOCKS_PER_SEC < sec)
         {
+            //That uses system resources, better solution should be possible
             end = clock ();
         }
     }
@@ -23,6 +75,49 @@ void sleep (unsigned int sec)
 #endif /* _WIN32 */
 }
 
+unsigned long Time::clock (bool sec)
+{
+
+    clock_t clk = clock();
+
+    //0 is an error code here : information not availalbe
+    unsigned long res = 0;
+
+    if ( clk != -1 )
+    {
+        res = (unsigned long)clk;
+        if ( sec)
+        {
+            res= (unsigned long) (res/ CLOCKS_PER_SEC) ;
+        }
+    }
+    //lk == -1 -> information nto available we return 0
+
+    return res;
+}
+
+Time Time::now()
+{
+    return Time(time(NULL));
+
+}
+
+std::string Time::localascii()
+{
+    return std::string(ctime( & tt ) );
+}
+
+Date Time::GMT()
+{
+    Date gmt(gmtime( & tt ));
+    return gmt;
+}
 
 
+Date Time::local()
+{
+    Date loc(localtime( & tt ));
+    return loc;
+}
 
+}//Core
